537a764b4156083e47b95af120ac8ad1fa82fd6a
Vetliniak <wetliniak@gmail.com>
2020-11-13 00:24:22 +0100

    try1

--- zajecia.py
+++ zajecia.py
@@ -7,7 +7,3 @@ list_to_sort = [[3, 2, 3], [2, 0, 2], [3, 0, 1]]
 list_to_sort.sort(key=sort_recipe, reverse=False)
 print(list_to_sort)
 
-list_to_sort_2 = [[3, 2, 3], [2, 0, 2], [3, 0, 1], [3, 1, 0]]
-
-list_to_sort_2.sort(key=sort_recipe, reverse=False)
-print(list_to_sort_2)
